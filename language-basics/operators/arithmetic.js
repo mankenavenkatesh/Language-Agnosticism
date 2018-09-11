7958efe236e74d74ae119c287cc853f2fab932eb
mankenavenkatesh <vmankena@BANL15013fa7c.local>
2018-09-11 09:36:12 +0530

    adding operators

---
+++ language-basics/operators/arithmetic.js
@@ -0,0 +1,10 @@
+var x = 100;
+var y = 50;
+
+console.log("x="+ x);
+console.log("y="+y);
+console.log("x + y = " + (x+y));
+console.log("x - y ="+ (x - y));
+console.log("x * y = "+ (x * y));
+console.log("x / y =" + (x / y));
+console.log("x % y =" + (x % y));
