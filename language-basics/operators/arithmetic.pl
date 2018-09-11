bdf435f8e6fc008bd55060156f929e612c4cfada
mankenavenkatesh <vmankena@BANL15013fa7c.local>
2018-09-11 10:00:31 +0530

    adding operators

---
+++ language-basics/operators/arithmetic.pl
@@ -0,0 +1,20 @@
+$a = 21;
+$b = 10;
+
+print "Value of \$a = $a and value of \$b=$b\n";
+
+$c = $a + $b;
+print 'Value of $a + $b =' . $c . "\n";
+
+$c = $a - $b;
+print 'Value of $a - $b=' . $c . "\n";
+
+$c = $a * $b;
+print 'Value of $a * $b=' . $c . "\n";
+
+$c = $a / $b;
+print 'Value of $a / $b=' . $c . "\n";
+
+$c = $a % $b;
+
+print 'Value of $a % $b='. $c. "\n";
