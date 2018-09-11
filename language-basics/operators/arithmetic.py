b5e3da307c98e70fc2558cf3fd68383681cc3f97
mankenavenkatesh <vmankena@BANL15013fa7c.local>
2018-09-11 10:12:33 +0530

    adding operators

---
+++ language-basics/operators/arithmetic.py
@@ -0,0 +1,22 @@
+a = 21;
+b = 10;
+c = 0;
+
+print 'a=', a
+print 'b=', b
+c = a + b;
+print 'a +b=', c
+c = a-b;
+print 'a -b =', c
+c = a* b;
+print 'a * b=', c
+c = a /b;
+print 'a/b=', c
+c = a%b;
+print 'a % b=', c
+
+a = 2;
+b = 3;
+print 'a=', a
+print 'b=', b
+print 'a ** b=', (a ** b)
