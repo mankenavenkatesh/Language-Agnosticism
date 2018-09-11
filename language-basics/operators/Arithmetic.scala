828ec3d4041c3ccb067d81020fe7a4b99a4b2c76
mankenavenkatesh <vmankena@BANL15013fa7c.local>
2018-09-11 11:44:12 +0530

    adding arithmetic operations

---
+++ language-basics/operators/Arithmetic.scala
@@ -0,0 +1,13 @@
+object Arithmetic {
+    def main(args: Array[String]){
+        var a = 20;
+        var b = 10;        
+        println("a = " + a);
+        println("b = " + b);
+        println("a + b =" + (a + b));
+        println("a - b =" + (a - b));
+        println("a * b =" + (a * b));
+        println("a /b =" + (a / b));
+        println(" a % b=" + (a % b));
+    }
+}
