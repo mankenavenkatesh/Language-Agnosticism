ec6247f0e2f8fc5f9bce9bf3e2530b8f732a137d
mankenavenkatesh <vmankena@BANL15013fa7c.local>
2018-09-11 11:35:00 +0530

    adding operators

---
+++ language-basics/operators/arithmetic.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let a = 4;
+    let b = 3;
+    println!("a + b={}", a+b);    
+    println!("a - b = {}", a -b);
+    println!("a * b = {}", a * b);
+    println!("a / b = {}", a / b);
+    println!("a % b = {}", a % b);
+}
