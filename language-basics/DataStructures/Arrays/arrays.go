6b9a1cdda5b6a2bfd12ad8dd313601a1cd002fb1
venkatesh mankena <mankenavenkatesh@gmail.com>
2020-04-15 11:11:28 +0530

    Array initialization

---
+++ language-basics/DataStructures/Arrays/arrays.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var arr [10]int
+	var i, j int
+
+	for i = 0; i < 10; i++ {
+		arr[i] = i + 1
+	}
+
+	for j = 0; j < 10; j++ {
+		fmt.Println(arr[j])
+	}
+}
