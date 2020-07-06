5801f36fc9119b595dc98c466920b4f8b0ca6e4f
venkatesh mankena <mankenavenkatesh@gmail.com>
2020-07-06 09:38:15 +0530

    data operations - functions

--- Programming-Constructs/DataOperations/Functions/main.rs
+++ Programming-Constructs/DataOperations/Functions/main.rs
@@ -2,6 +2,8 @@ fn main() {
     println!("Hello !world");
     another_function();
     another_function1(10, 4);
+    let s = sum(1,2);
+    println!("sum of 1 and 2 is {}",s)
 }
 
 fn another_function() {
@@ -12,3 +14,6 @@ fn another_function1(x: i32, y: i32) {
     println!("The value of x is: {} and y is:{}", x, y);
 }
 
+fn sum(a: i32, b: i32) -> i32 {
+    a+b
+}
