89820106b4d1a6c840312a46f80978fb266ccbf4
venkatesh mankena <mankenavenkatesh@gmail.com>
2020-07-06 09:20:27 +0530

    data operations - functions

--- Programming-Constructs/DataOperations/Functions/main.rs
+++ Programming-Constructs/DataOperations/Functions/main.rs
@@ -1,8 +1,14 @@
 fn main() {
     println!("Hello !world");
     another_function();
+    another_function1(10, 4);
 }
 
 fn another_function() {
     println!("Another function.");
 }
+
+fn another_function1(x: i32, y: i32) {
+    println!("The value of x is: {} and y is:{}", x, y);
+}
+
